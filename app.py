fd44a5d2da360509ff3924d61a4b89b15ef5ca3d
rzk <18268506614@163.com>
2026-06-06 17:52:27 +0800

    接入数据库并新增用户面板和账号登录注册功能

--- app.py
+++ app.py
@@ -1,14 +1,100 @@
-#导入包
 import requests
 import openai
-from flask import Flask,render_template
+import pymysql
+from flask import Flask,render_template,request,jsonify,session
 
 
 app=Flask(__name__)
+app.secret_key = 'fauihfaiefjalieuhfkj'
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+#登录路由
+@app.route("/index/login",methods=['POST'])
+def login():
+    data=request.get_json()
+    username=data['username']
+    password=data['password']
+    connect_login = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='ren20090920',
+        database='ai_learn',
+        charset='utf8mb4',
+    )
+    cursor = connect_login.cursor()
+    cursor.execute(
+        "select id,username,password from users where username = %s",
+        (username,)
+    )
+    user = cursor.fetchone()
+    cursor.close()
+    connect_login.close()
+    if user is None:
+        return jsonify({
+            'success':False,
+            'message':'用户不存在',
+            'needregister':True
+        })
+    user_id,db_username,db_password=user
+    if password != db_password:
+        return jsonify({'success':False,'message':'密码错误'})
+    session['user_id'] = user_id
+    session['username'] = db_username
+    return jsonify({'success':True,'message':'旅行者,欢迎回来!','username':db_username})
+
+#注册路由
+@app.route("/index/register", methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    if len(password)<6:
+        return jsonify({
+            'success':False,
+            'message':'密码必须大于6个字符'
+        })
+    connect_register = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='ren20090920',
+        database='ai_learn',
+        charset='utf8mb4',
+    )
+    cursor = connect_register.cursor()
+    #查询用户名是否已经存在
+    cursor.execute('select id from users where username = %s',(username,))
+    if cursor.fetchone() is not None:
+        cursor.close()
+        connect_register.close()
+        return jsonify({'success':False,'message':'用户名已存在'})
+    #在sql中加入新用户
+    cursor.execute(
+        'insert into users (username,password) values(%s,%s)',
+        (username,password)
+    )
+    connect_register.commit()
+    new_user_id = cursor.lastrowid
+    cursor.close()
+    connect_register.close()
+    session['user_id'] = new_user_id
+    session['username'] = username
+    return jsonify({'success':True,'message':'注册成功!','username':username})
+
+#检测登录状态路由
+@app.route("/index/check_login")
+def check_login():
+    if 'user_id' in session:
+        return jsonify({'loggedin':True,'username':session['username']})
+    return jsonify({'loggedin': False})
+
+#退出登录
+@app.route("/index/logout")
+def logout():
+    session.clear()
+    return jsonify({'success': True,'message':'已退出'})
 
 if __name__=="__main__":
     app.run(debug=True)
--- static/login.js
+++ static/login.js
@@ -1,7 +1,109 @@
+let is_register = false
 //打开弹窗
 document.getElementById('login_button').addEventListener('click',function(e){
 	e.preventDefault();
-	document.querySelector('.Mask_layer').classList.add('show');
-	document.querySelector('.model').classList.add('show');
+	document.getElementById('Mask_layer').classList.add('show');
+	document.getElementById('login_model').classList.add('show');
+	document.getElementById('login_model').classList.remove('register')
+	is_register = false
 });
+
 //关闭弹窗
+document.getElementById('close').addEventListener('click',function(e){
+	e.preventDefault();
+	document.getElementById('Mask_layer').classList.remove('show');
+	document.getElementById('login_model').classList.remove('show');
+})
+
+//获取输入框中的用户名和密码
+document.getElementById('login_submit').addEventListener('click',async function(e){
+	const username = document.getElementById('login_username').value.trim();
+	const password = document.getElementById('login_password').value.trim();
+	if (!username || !password) {
+		alert('用户名或密码不能为空!');
+		return;
+	}
+	if (is_register==true){
+		const repeat_password = document.getElementById('repeat_password').value.trim();
+		if (!repeat_password){alert('请再次输入密码');return;}
+		if (repeat_password !== password){
+			alert('两次密码不一致');
+			document.getElementById('repeat_password').value='';
+			return;
+			}
+		const respon = await fetch('/index/register',{
+			method:'POST',
+			headers:{'Content-Type':'application/json'},
+			body:JSON.stringify({username,password}),
+		});
+		const result = await respon.json();
+		alert(result.message)
+		if (result.success){
+			document.getElementById('Mask_layer').classList.remove('show');
+			document.getElementById('login_model').classList.remove('show');
+			user_button(result.username);
+		}else{
+			document.getElementById('login_password').value='';
+			document.getElementById('login_password').focus();
+			document.getElementById('repeat_password').value='';
+		}
+		return;
+	}
+	const respon = await fetch('/index/login',{
+		method:'POST',
+		headers:{'Content-Type':'application/json'},
+		body:JSON.stringify({username:username,password:password})
+	});
+	const result = await respon.json();
+	if (result.success) {
+		alert(result.message);
+		document.getElementById('Mask_layer').classList.remove('show');
+		document.getElementById('login_model').classList.remove('show')
+		user_button(result.username);
+	}else if (result.needregister){
+		if (confirm('用户不存在,是否注册')){
+			document.getElementById('login_model').classList.add('register')
+			is_register=true
+			document.getElementById('repeat_password').focus();
+		}
+	}else{
+		alert(result.message);
+		document.getElementById('login_password').value='';
+		document.getElementById('login_password').focus();
+	}
+});
+
+// 页面自动检查是否登录
+window.addEventListener('DOMContentLoaded', async function() {
+	const resp = await fetch('/index/check_login');
+	const data = await resp.json();
+	if (data.loggedin){
+		user_button(data.username);
+	}
+});
+
+//登录按钮变为用户
+function user_button(username) {
+	    document.getElementById('login_button').style.display = 'none';
+		document.getElementById('user_menu').style.display = 'block';
+		document.getElementById('username').textContent = username;
+};
+
+document.getElementById('avatar_button').addEventListener('click',function(e){
+	e.preventDefault();
+	e.stopPropagation();
+	document.getElementById('main_menu').classList.toggle('show');
+});
+
+window.addEventListener('click',function(e){
+	const menu = document.getElementById('user_menu');
+	if (!menu.contains(e.target)){
+		document.getElementById('main_menu').classList.remove('show');
+	}
+});
+
+//退出登录
+document.getElementById('logout_button').addEventListener('click',async function(){
+	await fetch('/index/logout');
+	location.reload();
+})
